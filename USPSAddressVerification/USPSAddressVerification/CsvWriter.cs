5f5c4901904202c60009fc553fa31819d91254c9
Nidesh Chitrakar <nideshchitrakar@bennington.edu>
2018-01-09 12:28:14 -0500

    csvwriter init

---
+++ USPSAddressVerification/USPSAddressVerification/CsvWriter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace USPSAddressVerification
+{
+    public class CsvWriter : StreamWriter
+    {
+        private string filename;
+
+        public CsvWriter(Stream stream) : base(stream)
+        {
+        }
+
+        public CsvWriter(string filename) : base(filename)
+        {
+        }
+
+        //public CsvWriter(string filename)
+        //{
+        //    this.filename = filename;
+        //}
+
+        public void writeHeader()
+        {
+            
+        }
+
+        public void appendRow()
+        {
+            
+        }
+
+    }
+}
